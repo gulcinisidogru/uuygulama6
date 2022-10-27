3b611ab29ceced597b6bf9e4b29ac826b39bac1f
gulci <gulci@LAPTOP-KPNR185M>
2022-10-27 16:29:25 +0300

    Proje dosyası ekle.

---
+++ uuygulama6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uuygulama6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Derece Değerini Giriniz: ");
+            double derece = Convert.ToDouble(Console.ReadLine());
+            double radyan = derece * Math.PI / 180;
+            double grad = (derece / 400) * 360;
+            Console.WriteLine("{0} Derece = {1} Radyan = {2} Grad", derece, radyan, grad);
+
+
+
+        }
+    }
+}
